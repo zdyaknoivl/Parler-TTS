b9a4f871f6bf924538036aac73b1b06b5b0d15e8
zdyaknoivl <itsoke@mail.ru>
2025-01-16 04:07:37 +0300

    conf intervals of inference speed added

--- metrics/inference_speed.py
+++ metrics/inference_speed.py
@@ -5,6 +5,8 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import time
 from collections import defaultdict
+import numpy as np
+from scipy.stats import norm
 
 
 class InferenceSpeed:
@@ -22,8 +24,8 @@ class InferenceSpeed:
 
         return end_time - start_time
 
-    def plot_inference_speed(self):
-        average_speed = defaultdict(lambda: (0, 0))
+    def plot_inference_speed(self, alpha=0.05):
+        average_speed = defaultdict(lambda: (0, 0, 0))
 
         for text in tqdm(self.dataset['train']['transcription_normalised']):
             len_text = len(word_tokenize(text))
@@ -31,15 +33,35 @@ class InferenceSpeed:
                 continue
 
             t = self.__calculate_generation_time(text)
-            average, num = average_speed[len_text]
-            average_speed[len_text] = (
-                (average * num + t) / (num + 1), num + 1)
+            average, count, variance = average_speed[len_text]
+
+            count += 1
+            new_average = (average * (count - 1) + t) / count
+            new_variance = ((variance * (count - 1)) +
+                            (t - new_average) * (t - average)) / count
+
+            average_speed[len_text] = (new_average, count, new_variance)
 
         x, y = zip(*sorted(average_speed.items()))
+        means = [item[0] for item in y]
+        counts = [item[1] for item in y]
+        variances = [item[2] for item in y]
 
-        plt.plot(x, [item[0] for item in y])
-        plt.title("Inference speed")
-        plt.xlabel("Tokens count")
-        plt.ylabel("Inference time")
+        confidence_intervals_lower = []
+        confidence_intervals_upper = []
+        for i in range(len(means)):
+            std_error = np.sqrt(variances[i] / counts[i])
+            z_value = norm.ppf(1 - alpha / 2)
+            ci = z_value * std_error
+            confidence_intervals_lower.append(means[i] - ci * (counts[i] > 1))
+            confidence_intervals_upper.append(means[i] + ci * (counts[i] > 1))
+
+        plt.plot(x, means, label="Average Inference Time", color="tab:orange")
+        plt.fill_between(x, confidence_intervals_lower,
+                         confidence_intervals_upper, color="tab:orange", alpha=0.3)
+
+        plt.title("Inference Speed with Confidence Intervals")
+        plt.xlabel("Tokens Count")
+        plt.ylabel("Inference Time (seconds)")
         plt.grid(True)
         plt.show()
